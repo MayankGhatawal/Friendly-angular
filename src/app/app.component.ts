a14bc62db24e0f6173a4a0d288405aa606bc84de
MayankGhatawal <mayank1ghatawal@gmail.com>
2024-09-20 20:13:57 +0530

    main

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,11 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular';
+  title = 'Mayank Ghatawal';
+
+  constructor(){
+    setTimeout(()=>{
+      this.title = 'Angular App';
+    }, 2000);
+  }
 }
